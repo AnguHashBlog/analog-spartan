09d9b2d59228a511323be61c698760bfb1bafbf1
Esther White <64324417+monacodelisa@users.noreply.github.com>
2024-03-18 19:44:54 +0000

    tailwind css works and favicon added

--- analog-spartan-app/src/app/pages/index.page.ts
+++ analog-spartan-app/src/app/pages/index.page.ts
@@ -1,49 +1,61 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-home',
-  standalone: true,
-  template: `
-    <div>
-      <a href="https://analogjs.org/" target="_blank">
-        <img alt="Analog Logo" class="logo analog" src="/analog.svg" />
-      </a>
-    </div>
+	selector: "app-home",
+	standalone: true,
+	template: `
 
-    <h2>Analog</h2>
+		<div>
+			<a href="https://analogjs.org/" target="_blank">
+				<img alt="Analog Logo" class="logo analog" src="/analog.svg" />
+			</a>
+		</div>
 
-    <h3>The fullstack meta-framework for Angular!</h3>
+		<h2>Analog</h2>
 
-    <div class="card">
-      <button type="button" (click)="increment()">Count {{ count }}</button>
-    </div>
+		<h3>The fullstack meta-framework for Angular!</h3>
+
+		<div class="card">
+			<button type="button" (click)="increment()">Count {{ count }}</button>
+		</div>
 
     <p class="read-the-docs">
-      For guides on how to customize this project, visit the
-      <a href="https://analogjs.org" target="_blank">Analog documentation</a>
-    </p>
-  `,
-  styles: [
-    `
-      .logo {
-        will-change: filter;
+			For guides on how to customize this project, visit the
+			<a href="https://analogjs.org" target="_blank">Analog documentation</a>
+		</p>
+
+    <h3 class="text-2xl font-bold underline">Hello also from Tailwind CSS!</h3>
+
+	`,
+	styles: [
+		`
+			.logo {
+				will-change: filter;
+        width: 15rem;
+        margin: 0 auto;
+			}
+			.logo:hover {
+				filter: drop-shadow(0 0 1rem #646cffaa);
+			}
+
+      h2 {
+        font-size: 2rem;
+        margin-bottom: 1rem;
       }
-      .logo:hover {
-        filter: drop-shadow(0 0 2em #646cffaa);
+			.read-the-docs {
+				color: #888;
+			}
+      h3 {
+        margin: 1rem 0;
       }
-      .logo.angular:hover {
-        filter: drop-shadow(0 0 2em #42b883aa);
-      }
-      .read-the-docs {
-        color: #888;
-      }
-    `,
-  ],
+
+		`,
+	],
 })
 export default class HomeComponent {
-  count = 0;
+	count = 0;
 
-  increment() {
-    this.count++;
-  }
+	increment() {
+		this.count++;
+	}
 }
